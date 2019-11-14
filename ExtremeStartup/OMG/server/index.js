57b691b2e072fb8ec101b59e13ad5124642f3aa6
Gunnar Kriik <gunnar.kriik@sparebank1.no>
2019-11-14 12:04:18 +0100

    quick fixes

--- ExtremeStartup/OMG/server/index.js
+++ ExtremeStartup/OMG/server/index.js
@@ -10,9 +10,13 @@ app.get('*', (req, res) => {
   console.log('  request.query', req.query);
   console.log('  request.headers', req.headers);
 
-  const match = req.query && req.query.q && req.query.q.match(/^(.+):\s(.*)$/)
-  if (match && match.length > 1 && match[2] === 'what is your name') {
-    res.send('OMG');
+  const match = req.query && req.query.q && req.query.q.match(/^([0-9a-z]*):\s(.*)$/)
+  if (match && match.length > 1 && match[2].indexOf('which of the following numbers is the largest') >= 0) {
+    const numbers = match[2].split(': ')[1].split(',').sort().map(i => parseInt(i));
+    res.send('' + Math.max(...numbers));
+  } else if (match && match.length > 1 && match[2].indexOf('what is') >= 0) {
+    const numbers = match[2].match(/(\d+)\splus\s(\d+)/);
+    res.send('' + (parseInt(numbers[1]) + parseInt(numbers[2])));
   } else {
     res.send('OK');
   }  
