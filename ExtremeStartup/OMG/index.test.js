a74a8e1f857efccf45a18224c9f2904b2860449c
Gunnar Kriik <gunnar.kriik@sparebank1.no>
2019-11-14 11:04:45 +0100

    Some fixes

--- ExtremeStartup/OMG/index.test.js
+++ ExtremeStartup/OMG/index.test.js
@@ -2,27 +2,21 @@
 const request = require('supertest')
 const app = require('./server')
 
-describe('Get Hello World', () => {
+describe('Extreme startup server', () => {
   it('should create a new post', async () => {
     const res = await request(app)
       .get('/')
     expect(res.statusCode).toEqual(200)
-    //console.log('', res)
-    expect(res.text).toEqual('Hello World!')
+    expect(res.text).toEqual('OK')
   })
-})
 
-describe('Respond with name', () => {
-  it('should create a new post', async () => {
+  it('should respond with name', async () => {
     const res = await request(app)
       .get('/?q=3cd31d80:%20what%20is%20your%20name')
     expect(res.statusCode).toEqual(200)
-    //console.log('', res)
-    expect(res.text).toEqual('Hello World!')
+    expect(res.text).toEqual('OMG')
   })
 })
-
-
 
 describe('Sample Test', () => {
   it('should test that true === true', () => {
--- ExtremeStartup/OMG/server/index.js
+++ ExtremeStartup/OMG/server/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const port = 3000;
 
-app.use(morgan('dev'));
+// http://10.226.7.52:3000/players/597b7f10
+
+app.use(morgan('common'));
 
 app.get('*', (req, res) => {
   console.log('  request.query', req.query);
   console.log('  request.headers', req.headers);
 
-  const match = req.query.q.match(/^(.+):(.*)$/)
-  if (match[2] === 'what is your name') {
-    //res.send('OMG');
+  const match = req.query && req.query.q && req.query.q.match(/^(.+):\s(.*)$/)
+  if (match && match.length > 1 && match[2] === 'what is your name') {
     res.send('OMG');
   } else {
     res.send('OK');
   }  
-})
+});
 
 module.exports = app;
